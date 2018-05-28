7d2fe3dbefd2ae8609c2fb75927430e9bb8f59ec
Michael Walton <mwalton.us@gmail.com>
2018-05-27 23:14:06 -0700

    Refactor data types

--- lib/query.h
+++ lib/query.h
@@ -23,15 +23,16 @@ class Query
 			NoCommand,       // Used to indicate that the given parameter does not have an associated command
 		};
 
+		// TODO: Make types suck less
 		// Type defines these data structures so implementation is easier to read/change
-		typedef Model row_t;           /// Represents a record produced by a query
+		typedef Model row_t;                 /// Represents a record produced by a query
 		typedef std::vector<Model> table_t;  /// Collection of records produced by a query
 
 		/// Collection of fields + aggregate commands
-		typedef std::vector<std::tuple<std::string, Query::Command>> select_command_t;
+		typedef std::map<std::string, Query::Command> select_args_t;
 
 		/// Collection of commands + arguments
-		typedef std::vector<std::tuple<Query::Command, std::string>> command_queue_t;
+		typedef std::map<Query::Command, std::string> command_map_t;
 
 		/// Construction
 		Query() = delete;
@@ -51,20 +52,26 @@ class Query
 		void Filter(Query::table_t& queryData, const std::string& filter);
 		void Aggregate(Query::table_t& queryData, const std::string& groupField);
 
+		/// Returns true or false for whether the given record passes the filter
+		static bool EvaluateFilterString(const row_t& record, const std::string& logicString);
+
 		/// Creates an ordered collection of commands to perform from the given query string.
-		static Query::command_queue_t ParseQueryString(const std::string& queryString);
+		static command_map_t ParseQueryString(const std::string& queryString);
 
 		/// Parse the select command argument for fields and their respective aggregate functions.
-		static Query::select_command_t ParseSelectCommandArgs(const std::string& commandArgs);
+		static select_args_t ParseSelectCommandArgs(const std::string& commandArgs);
 
 		/// Map of known strings to their related Query functions for parsing query strings.
 		static const std::map<std::string, Query::Command> m_knownCommands;
 
 		/// Ordered collection of commands to be performed when QueryCommand is called.
-		command_queue_t m_commandChain;
+		Query::command_map_t m_commandChain;
 
 		/// Cache the fields and their aggregate functions specified in the select command.
-		Query::select_command_t m_selectCommands;
+		Query::select_args_t m_selectArgs;
+
+		/// TODO: something
+		Query::select_args_t m_aggregateCommands;
 };
 
 #endif
